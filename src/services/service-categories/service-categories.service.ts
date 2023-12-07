f957b2543ee80de76c482e102c8763e90467f499
Amina ECH-CHAKFY <117484543+Amina-ECH-CHAKFY@users.noreply.github.com>
2023-12-07 18:54:09 +0100

    master_ServiceCategorie

--- src/services/service-categories/service-categories.service.ts
+++ src/services/service-categories/service-categories.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ServiceCategoryEntity } from '../../migrations/service-category.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ServiceCategoriesService {}
+export class ServiceCategoriesService {
+  constructor(
+    @InjectRepository(ServiceCategoryEntity)
+    private readonly serviceCategoryRepository: Repository<ServiceCategoryEntity>,
+  ) {}
+
+  async getAllServiceCategories(): Promise<ServiceCategoryEntity[]> {
+    return await this.serviceCategoryRepository.find();
+  }
+
+  async getServiceCategoryById(id: number): Promise<ServiceCategoryEntity> {
+    return await this.serviceCategoryRepository.findOne({
+      where: { idServiceCategory: id },
+    });
+  }
+
+  async createServiceCategory(
+    serviceCategory: ServiceCategoryEntity,
+  ): Promise<ServiceCategoryEntity> {
+    return await this.serviceCategoryRepository.save(serviceCategory);
+  }
+
+  async updateServiceCategory(
+    id: number,
+    serviceCategory: ServiceCategoryEntity,
+  ): Promise<ServiceCategoryEntity> {
+    const existingServiceCategory = await this.getServiceCategoryById(id);
+
+    if (!existingServiceCategory) {
+      throw new Error('Service category not found');
+    }
+
+    existingServiceCategory.categoryName = serviceCategory.categoryName;
+    existingServiceCategory.categoryDescription = serviceCategory.categoryDescription;
+
+    return await this.serviceCategoryRepository.save(existingServiceCategory);
+  }
+
+  async deleteServiceCategory(id: number): Promise<void> {
+    const existingServiceCategory = await this.getServiceCategoryById(id);
+
+    if (!existingServiceCategory) {
+      throw new Error('Service category not found');
+    }
+
+    await this.serviceCategoryRepository.delete(id);
+  }
+}
