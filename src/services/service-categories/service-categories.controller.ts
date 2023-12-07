3caf7c42bdfb81f2f13ad83ad56b122dc3a51343
Amina ECH-CHAKFY <117484543+Amina-ECH-CHAKFY@users.noreply.github.com>
2023-12-07 19:22:47 +0100

    master_ServiceCategorie

--- src/services/service-categories/service-categories.controller.ts
+++ src/services/service-categories/service-categories.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
 
-@Controller('service-categories')
-export class ServiceCategoriesController {}
+import { Controller, Get, Post, Put, Delete, Param, Body, } from '@nestjs/common';
+import { ServiceCategoriesService } from './service-categories.service';
+import { ServiceCategoryEntity } from '../../migrations/service-category.entity';
+
+  
+  @Controller('service-categories')
+  export class ServiceCategoriesController {
+    constructor(
+      private readonly serviceCategoriesService: ServiceCategoriesService,
+    ) {}
+  
+    @Get()
+    async getAllServiceCategories(): Promise<ServiceCategoryEntity[]> {
+      return await this.serviceCategoriesService.getAllServiceCategories();
+    }
+  
+    @Get(':id')
+    async getServiceCategoryById(@Param('id') id: number): Promise<ServiceCategoryEntity> {
+      return await this.serviceCategoriesService.getServiceCategoryById(id);
+    }
+  
+    @Post()
+    async createServiceCategory(
+      @Body() serviceCategory: ServiceCategoryEntity,
+    ): Promise<ServiceCategoryEntity> {
+      return await this.serviceCategoriesService.createServiceCategory(serviceCategory);
+    }
+  
+    @Put(':id')
+    async updateServiceCategory(
+      @Param('id') id: number,
+      @Body() serviceCategory: ServiceCategoryEntity,
+    ): Promise<ServiceCategoryEntity> {
+      return await this.serviceCategoriesService.updateServiceCategory(id, serviceCategory);
+    }
+  
+    @Delete(':id')
+    async deleteServiceCategory(@Param('id') id: number): Promise<void> {
+      await this.serviceCategoriesService.deleteServiceCategory(id);
+    }
+  }
