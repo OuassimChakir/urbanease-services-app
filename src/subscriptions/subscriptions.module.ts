09f3b24a44ba976774a1812e33f25dfb1d456f10
Ouassim Chakir <oua.chakir@edu.umi.ac.ma>
2023-12-13 23:56:51 +0100

    master_subscriptions

--- src/subscriptions/subscriptions.module.ts
+++ src/subscriptions/subscriptions.module.ts
@@ -3,9 +3,13 @@ import { SubscriptionsController } from './subscriptions.controller';
 import { SubscriptionsService } from './subscriptions.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SubscriptionEntity } from '../migrations/subscription.entity';
+import { ClientEntity } from '../migrations/client.entity';
+import { PlanEntity } from '../migrations/plan.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SubscriptionEntity])],
+  imports: [
+    TypeOrmModule.forFeature([SubscriptionEntity, ClientEntity, PlanEntity]),
+  ],
   controllers: [SubscriptionsController],
   providers: [SubscriptionsService],
 })
--- src/subscriptions/subscriptions.service.ts
+++ src/subscriptions/subscriptions.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ClientEntity } from '../migrations/client.entity';
+import { Repository } from 'typeorm';
+import { PlanEntity } from '../migrations/plan.entity';
+import { SubscriptionEntity } from '../migrations/subscription.entity';
 
 @Injectable()
-export class SubscriptionsService {}
+export class SubscriptionsService {
+  constructor(
+    @InjectRepository(ClientEntity)
+    private clientRepo: Repository<ClientEntity>,
+    @InjectRepository(PlanEntity)
+    private planRepo: Repository<PlanEntity>,
+    @InjectRepository(ClientEntity)
+    private subscriptionRepo: Repository<SubscriptionEntity>,
+  ) {}
+}
