da7a8d3f5312b3149d6dea179c3750f5c5fc4899
Ouassim Chakir <oua.chakir@edu.umi.ac.ma>
2023-12-14 00:47:04 +0100

    master_payments

---
+++ src/payment-transactions/payment-status.enum.ts
@@ -0,0 +1,4 @@
+export enum PaymentStatusEnum {
+  PAID = 1,
+  UNPAID = 0,
+}
--- src/payment-transactions/payment-transactions.module.ts
+++ src/payment-transactions/payment-transactions.module.ts
@@ -3,9 +3,13 @@ import { PaymentTransactionsController } from './payment-transactions.controller
 import { PaymentTransactionsService } from './payment-transactions.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PaymentEntity } from '../migrations/payment.entity';
+import { UserEntity } from '../migrations/user.entity';
+import { SubscriptionEntity } from '../migrations/subscription.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PaymentEntity])],
+  imports: [
+    TypeOrmModule.forFeature([PaymentEntity, UserEntity, SubscriptionEntity]),
+  ],
   controllers: [PaymentTransactionsController],
   providers: [PaymentTransactionsService],
 })
